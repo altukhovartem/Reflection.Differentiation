92be88d2d2628ebf024c04f35482cbd51d37c2b6
Artem <altukhov.artem.dev@gmain.com>
2018-09-24 23:09:57 +0300

    second commit

--- Reflection.Differentiation.csproj/Algebra.cs
+++ Reflection.Differentiation.csproj/Algebra.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Reflection.Differentiation
 {
-   
+    public class Algebra
+    {
+        internal static Expression<Func<double, double>> Differentiate(Expression<Func<double, double>> function)
+        {
+            Expression<Func<double, double>> resultExp = null;
+            var qwe = function.Compile();
+            var xx = qwe(10);
+            ExpressionType expType = function.Body.NodeType; 
+            switch (expType)
+            {
+                //case (ExpressionType.Constant): return 0; break; 
+
+            }
+
+            throw new NotImplementedException();
+        }
+    }
 }
