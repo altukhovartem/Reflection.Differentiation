e88c622b243189da4dc9986682d2167bfc9117a6
Altukhov Artem <altukhov.artem.dev@gmail.com>
2018-10-02 08:50:44 +0300

    Fourth Commit

--- Reflection.Differentiation.csproj/Algebra.cs
+++ Reflection.Differentiation.csproj/Algebra.cs
@@ -2,43 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Reflection.Differentiation
 {
-    public class Algebra
+    public static class Algebra
     {
         internal static Expression<Func<double, double>> Differentiate(Expression<Func<double, double>> function)
         {
-            Expression<Func<double, double>> resultExp = null;
-            var oldParameters = function.Parameters;
-            var oldBody = function.Body;
-            var nodeType = function.NodeType;
+            return Expression.Lambda<Func<double, double>>(function.Body.Differentiate(), function.Parameters);
+        }
 
-            ParameterExpression newParams = null;
+        public static Expression Differentiate(this Expression function)
+        {
             Expression newBody = null;
 
-            ExpressionType expType = function.Body.NodeType; 
-            switch (expType)
+
+            ExpressionType expType = function.NodeType; 
+            switch (function)
             {
-                case (ExpressionType.Constant):
-                    newParams = oldParameters.FirstOrDefault();
+                case ConstantExpression _:
                     newBody = Expression.Constant(0.0);
                     break;
-                case (ExpressionType.Parameter):
-                    newParams = oldParameters.FirstOrDefault();
+                case ParameterExpression _:
                     newBody = Expression.Constant(1.0);
                     break;
-                case ExpressionType type when type.Equals(ExpressionType.Multiply):
-                    newParams = oldParameters.FirstOrDefault();
-                    newBody = Expression.Constant(5.0);
-                    break; 
+                case BinaryExpression be when (be.NodeType == ExpressionType.Multiply):
+                    newBody = Expression.Add(Expression.Multiply(be.Right.Differentiate(), be.Left), Expression.Multiply(be.Left.Differentiate(), be.Right));
+                    break;
+                case BinaryExpression be when (be.NodeType == ExpressionType.Divide):
+                    newBody = be.Right;
+                    break;
+                case BinaryExpression be when (be.NodeType == ExpressionType.Add):
+                    newBody = Expression.Add(be.Left.Differentiate(), be.Right.Differentiate());
+                    break;
+                case BinaryExpression be when (be.NodeType == ExpressionType.Subtract):
+                    newBody = be.Right;
+                    break;
+                case MethodCallExpression be when (be.NodeType == ExpressionType.Call && be.Method.Name == "Sin"):
+           
+
+
+                    break;
+                case MethodCallExpression be when (be.NodeType == ExpressionType.Call && be.Method.Name == "Cos"):
+                    break;
             }
 
-            resultExp = Expression.Lambda<Func<double, double>>(newBody, newParams);
-
-            return resultExp; 
+            return newBody; 
         }
     }
 }
